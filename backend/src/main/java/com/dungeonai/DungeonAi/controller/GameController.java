cee087de39bce3711854e5e7b45d4daed973c251
Sam <sgm@iastate.edu>
2023-02-03 22:15:50 -0600

    pulls random photo and saves

--- backend/src/main/java/com/dungeonai/DungeonAi/controller/GameController.java
+++ backend/src/main/java/com/dungeonai/DungeonAi/controller/GameController.java
@@ -2,10 +2,14 @@ package com.dungeonai.DungeonAi.controller;
 
 import com.dungeonai.DungeonAi.entity.Game;
 import com.dungeonai.DungeonAi.repository.GameRepository;
+import com.dungeonai.DungeonAi.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -16,9 +20,17 @@ public class GameController {
     @Autowired
     GameRepository gameRepository;
 
+    @Autowired
+    CharacterService characterService;
+
     @PostMapping
-    public ResponseEntity<Game> create(@RequestBody Game game) {
+    public ResponseEntity<Game> create(@RequestBody Game game) throws IOException {
         Game savedGame = gameRepository.save(game);
+        try {
+            characterService.createCharacter();
+        } catch (IOException e) {
+            System.out.println("shit don\'t work");
+        }
         return ResponseEntity.ok(savedGame);
     }
 
