3241e5c0727207b2c6123c034d934eccfca5d8ff
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-02-03 18:38:03 -0600

    Init React app

---
+++ frontend/dungeonai/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    // React router to different routes
+    <div className="App">
+      <p>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
