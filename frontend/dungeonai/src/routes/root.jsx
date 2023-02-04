dd9e1f3fef1b1426b5002fb95e5bff475457f515
jorgoose <78634823+jorgoose@users.noreply.github.com>
2023-02-03 19:51:48 -0600

    Added app navbar logo and color to body.

--- frontend/dungeonai/src/routes/root.jsx
+++ frontend/dungeonai/src/routes/root.jsx
@@ -1,33 +1,12 @@
+import React from "react";
+
+import Navbar from "../components/Navbar";
+
 export default function Root() {
   return (
-    <>
-      <div id="sidebar">
-        <h1>React Router Contacts</h1>
-        <div>
-          <form id="search-form" role="search">
-            <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
-            />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <form method="post">
-            <button type="submit">New</button>
-          </form>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <a href={`/games`}>View Games</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div id="detail"></div>
-    </>
+    <div>
+      <Navbar />
+      <div className="h-screen bg-slate-400"></div>
+    </div>
   );
 }
