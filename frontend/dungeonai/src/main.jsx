bbe4f63a9d7d72fa4f719e47326c6742439e15b9
djs1 <djs1@iastate.edu>
2023-02-04 10:33:20 -0600

    characterCreate Screen finished, needs form submit

--- frontend/dungeonai/src/main.jsx
+++ frontend/dungeonai/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import Root from "./routes/root";
 import Games from "./routes/games";
 import Character from "./routes/character";
+import CharacterCreate from "./routes/characterCreate";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
   {
     path: "/character",
     element: <Character />,
+  },
+  {
+    path: "/characterCreate",
+    element: <CharacterCreate />,
   }
 ]);
 
