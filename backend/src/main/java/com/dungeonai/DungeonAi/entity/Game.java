fcc7b4852f8e94de47cb34baa702f34e82c52a6d
Sam <sgm@iastate.edu>
2023-02-03 18:46:01 -0600

    created game POJO in backend

---
+++ backend/src/main/java/com/dungeonai/DungeonAi/entity/Game.java
@@ -0,0 +1,46 @@
+package com.dungeonai.DungeonAi.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+
+@Entity
+@Table(name = "games")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
